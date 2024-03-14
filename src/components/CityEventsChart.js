5494f5470409024ecec2a3a2e5a35e9dd4e9a874
Matthew <106117475+MLindbloom@users.noreply.github.com>
2024-03-13 21:42:14 -0700

    fix: CityEventsChart no decimals

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -48,7 +48,12 @@ const CityEventsChart = ({ allLocations, events }) => {
           interval={0}
           tick={{ dx: 20, dy: 40, fontSize: 14 }}
         />
-        <YAxis type='number' dataKey='count' name='Number of Events' />
+        <YAxis
+          type='number'
+          dataKey='count'
+          name='Number of Events'
+          allowDecimals={false}
+        />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name='A school' data={data} fill='#8884d8' />
       </ScatterChart>
